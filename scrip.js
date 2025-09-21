e80ac89ea8cae874a8af2b7725f4563e9e8ca4d0
Greimil <greimilnunez@gmail.com>
2025-09-21 11:25:06 -0400

    dark mode and responsiviness fix

--- scrip.js
+++ scrip.js
@@ -4,22 +4,44 @@ const menuBurger = document.getElementById("menuBurger");
 const btntoggletheme = document.getElementById("btn-toggle-theme");
 const navigationbar = document.querySelector(".navigationbar");
 const cards = document.querySelectorAll(".text-area");
-const profiletext = document.getElementById("profile")
+const profiletext = document.getElementById("profile");
+const body = document.querySelector("body");
 
 
-btnMenu.addEventListener("click", (e) => {
-  console.log("hola xd");
+function checkScreenSize() {
+  if (window.innerWidth <= 865) {
+
+    headerside.classList.add("hideShowMenu");
+    menuBurger.classList.remove("hideShowMenu");
+  } else {
+    
+    headerside.classList.remove("hideShowMenu");
+    menuBurger.classList.add("hideShowMenu");
+  }
+}
+
+
+checkScreenSize();
+
+
+window.addEventListener("resize", checkScreenSize);
+
+
+btnMenu.addEventListener("click", () => {
   headerside.classList.toggle("hideShowMenu");
   menuBurger.classList.toggle("hideShowMenu");
 });
+
 
 menuBurger.addEventListener("click", () => {
   headerside.classList.toggle("hideShowMenu");
   menuBurger.classList.toggle("hideShowMenu");
 });
 
-btntoggletheme.addEventListener("click", (e) => {
+
+btntoggletheme.addEventListener("click", () => {
   navigationbar.classList.toggle("darkmode");
+  body.classList.toggle("bodydark")
   
   cards.forEach((card) => {
     card.classList.toggle("darkmode");
